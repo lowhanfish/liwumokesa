4526aa2154c489abf6739a7f992efc046f624319
lowhanfish <lowhanfish@gmail.com>
2025-07-19 22:13:19 +0800

    change icon

--- website_desa/src/components/navbar.vue
+++ website_desa/src/components/navbar.vue
@@ -3,7 +3,7 @@
     <q-header elevated class="gradHijaukk">
       <q-toolbar class="bg-grey-9">
         <q-btn
-          v-if="checker "
+          v-if="checker"
           flat
           dense
           round
@@ -12,54 +12,51 @@
           icon="menu"
         />
 
-        <q-toolbar-title style="padding:10px">
-        <div class="text-left row">
-          
-          <div class="text-center" style="padding-top:0.1%">
-            <q-img
-              src="img/logx.jpeg"
-              spinner-color="primary"
-              spinner-size="90px"
-              style="width:40px"
-            />
-          </div>
-          <div>
-            <span style="padding:10px;font-size:22px; text-transform:uppercase;"  ><b>{{$store.state.KAT}} {{$store.state.DES_KEL}}</b>
-            </span>
-              <br>
-              <div style="margin-top:-15px">
-                <span style="padding-left:5%; font-size:14px;">KEC. {{$store.state.KECAMATAN}}</span>
-
+        <q-toolbar-title style="padding: 10px">
+          <div class="text-left row">
+            <div class="text-center" style="padding-top: 0.1%">
+              <q-img
+                src="img/logx.jpeg"
+                spinner-color="primary"
+                spinner-size="90px"
+                style="width: 45px; margin-top: 5px"
+              />
+            </div>
+            <div>
+              <span style="padding: 10px; font-size: 22px; text-transform: uppercase"
+                ><b>{{ $store.state.KAT }} {{ $store.state.DES_KEL }}</b>
+              </span>
+              <br />
+              <div style="margin-top: -15px">
+                <span style="padding-left: 5%; font-size: 14px"
+                  >KEC. {{ $store.state.KECAMATAN }}</span
+                >
               </div>
-
+            </div>
           </div>
-
-        </div>
         </q-toolbar-title>
         <!-- <q-toolbar-title style="padding:10px"><b>SIDAKCAMAT</b></q-toolbar-title>  -->
-        
-
 
         <div v-if="!checker">
           <ul>
             <template v-for="(data, index) in $store.state.menu">
-              <li v-if="data.type == 'single'" :key="'menu'+index">
-                <router-link :to="data.router">{{data.nama}}</router-link>
+              <li v-if="data.type == 'single'" :key="'menu' + index">
+                <router-link :to="data.router">{{ data.nama }}</router-link>
               </li>
 
-              <li v-if="data.type == 'multi'" :key="'menu'+index">
+              <li v-if="data.type == 'multi'" :key="'menu' + index">
                 <a href="javascript:void(0)">
-                  <b>{{data.nama}} <q-icon name="expand_more" /></b>
+                  <b>{{ data.nama }} <q-icon name="expand_more" /></b>
                   <q-menu transition-show="jump-down" transition-hide="jump-up">
                     <q-list style="min-width: 100px">
                       <q-item
                         clickable
                         v-for="(data1, index1) in data.submenu"
-                        :key="index+'submenu'+index1"
+                        :key="index + 'submenu' + index1"
                         :to="data1.router"
                       >
                         <q-item-section>
-                          {{data1.nama}}
+                          {{ data1.nama }}
                           <!-- <h1>hsdfkhkshdj</h1> -->
                         </q-item-section>
                       </q-item>
@@ -87,57 +84,82 @@
         spinner-color="primary"
         spinner-size="82px"
       /> -->
-      <div class="text-right shadow-2 bg-yellow-10" style="padding : 5px 20px 5px 0px; ">
+      <div class="text-right shadow-2 bg-yellow-10" style="padding: 5px 20px 5px 0px">
         <div @click="leftDrawerOpen = false">
-          <b style="color:black">
-          <q-icon name="keyboard_arrow_left" class="text-white" style="font-size: 35px;"/>
-          &nbsp; 
-          <span  class="text-white" style="font-size:10pt; ">TUTUP</span> 
+          <b style="color: black">
+            <q-icon
+              name="keyboard_arrow_left"
+              class="text-white"
+              style="font-size: 35px"
+            />
+            &nbsp;
+            <span class="text-white" style="font-size: 10pt">TUTUP</span>
           </b>
         </div>
       </div>
       <q-list class="rounded-borders">
-        <template  v-for="(data, index) in $store.state.menu">
-
-
-          <q-item clickable  :to="data.router" exact  v-if="data.type == 'single'" :key="'menu'+index">
+        <template v-for="(data, index) in $store.state.menu">
+          <q-item
+            clickable
+            :to="data.router"
+            exact
+            v-if="data.type == 'single'"
+            :key="'menu' + index"
+          >
             <q-item-section top avatar>
-              <q-avatar :color="data.color" :text-color="data.icon_color" :icon="data.icon" />
+              <q-avatar
+                :color="data.color"
+                :text-color="data.icon_color"
+                :icon="data.icon"
+              />
             </q-item-section>
             <q-item-section>
-              <q-item-label class="h_menu_drawer" :style="'color :' + data.text_color">{{data.nama}}</q-item-label>
+              <q-item-label class="h_menu_drawer" :style="'color :' + data.text_color">{{
+                data.nama
+              }}</q-item-label>
               <!-- <q-item-label caption lines="2">Dashboard</q-item-label> -->
             </q-item-section>
           </q-item>
 
-          <q-expansion-item :content-inset-level="1"  v-if="data.type == 'multi'" :key="'menu'+index">
+          <q-expansion-item
+            :content-inset-level="1"
+            v-if="data.type == 'multi'"
+            :key="'menu' + index"
+          >
             <template v-slot:header>
               <q-item-section avatar>
-                <q-avatar :icon="data.icon" :color="data.color" :text-color="data.icon_color" />
+                <q-avatar
+                  :icon="data.icon"
+                  :color="data.color"
+                  :text-color="data.icon_color"
+                />
               </q-item-section>
               <q-item-section class="h_menu_drawer" :style="'color :' + data.text_color">
-                {{data.nama}}
+                {{ data.nama }}
               </q-item-section>
             </template>
 
             <q-list :content-inset-level="0.5">
-
-              <q-item v-for="(data1, index1) in data.submenu" :key="index+'submenu'+index1" clickable :to="data1.router" :content-inset-level="0.5">
-                <q-item-section class="h_menu_drawer1"  :style="'color :' + data.text_color">- {{data1.nama}}</q-item-section>
+              <q-item
+                v-for="(data1, index1) in data.submenu"
+                :key="index + 'submenu' + index1"
+                clickable
+                :to="data1.router"
+                :content-inset-level="0.5"
+              >
+                <q-item-section
+                  class="h_menu_drawer1"
+                  :style="'color :' + data.text_color"
+                  >- {{ data1.nama }}</q-item-section
+                >
               </q-item>
-
-             </q-list>
+            </q-list>
           </q-expansion-item>
-
-
         </template>
       </q-list>
     </q-drawer>
   </div>
 </template>
-
-
-
 
 <script>
 export default {
@@ -150,8 +172,8 @@ export default {
       leftDrawerOpen: false,
       window: {
         width: 0,
-        height: 0
-      }
+        height: 0,
+      },
     };
   },
 
@@ -166,7 +188,7 @@ export default {
     handleResize() {
       this.window.width = window.innerWidth;
       this.window.height = window.innerHeight;
-    }
+    },
   },
   computed: {
     checker() {
@@ -178,8 +200,8 @@ export default {
       } else {
         return false;
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
